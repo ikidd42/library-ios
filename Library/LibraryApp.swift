5e29e36f6b13969efb05efde8039356f179a59a7
Ian Kidd <ibkidd42@gmail.com>
2026-02-11 19:10:37 -0500

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Ian on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
